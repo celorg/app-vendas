01de410973ea179d2f516710ad2e6b8d3ca8b8ad
celorg <marcelinho.ribeirog.2000@gmail.com>
2023-12-12 18:29:46 -0300

    feat: create modules login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { SafeAreaView, Text, View } from "react-native";
-import styled from "styled-components/native";
+import { SafeAreaView } from "react-native";
 
-const TextNew = styled.Text`
-    color: #ff8800;
-    font-size: 24px;
-`
+import Login from "./modules/login";
 
 const App = () => {
 
     return (
         <SafeAreaView>
-            <View>
-                <Text>Testando</Text>
-                <TextNew title="Titutlo teste" >Novo Teste</TextNew>
-            </View>
+            <Login />
         </SafeAreaView>
     )
 };
