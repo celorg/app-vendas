2e328d171a422148dbdeeafcebd97a9719fb37ac
celorg <marcelinho.ribeirog.2000@gmail.com>
2023-12-12 16:16:43 -0300

    feat: create app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, View } from "react-native"
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Testando</Text>
+            </View>
+        </SafeAreaView>
+    )
+};
+
+export default App;
