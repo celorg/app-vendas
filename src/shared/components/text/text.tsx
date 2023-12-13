f96fe723c0c524296923b04505ee0da778b91dc8
celorg <marcelinho.ribeirog.2000@gmail.com>
2023-12-13 20:39:00 -0300

    feat: insert types in Text

--- src/shared/components/text/text.tsx
+++ src/shared/components/text/text.tsx
@@ -6,21 +6,63 @@ import { useMemo } from "react";
 interface TextProps extends TextPropsNative{
     color?: string;
     type?: string;
+    font?: string;
 };
 
 const Text = ({ color, type, ...props }: TextProps) => {
 
-    const handleSize: string = useMemo(() => {
+    const fontSize: string = useMemo(() => {
         switch (type) {
-            case textTypes.TITLE:
-                return '32px';
+            case textTypes.TITLE_BOLD:
+            case textTypes.TITLE_LIGHT:
+            case textTypes.TITLE_REGULAR:
+                return '24px';
+            case textTypes.SUB_TITLE_BOLD:
+            case textTypes.SUB_TITLE_LIGHT:
+            case textTypes.SUB_TITLE_REGULAR:
+                return '20px';
+            case textTypes.BUTTON_BOLD:
+            case textTypes.BUTTON_LIGHT:
+            case textTypes.BUTTON_LIGHT:
+                return '18px';
+            case textTypes.PARAGRAPH_SMALL_BOLD:
+            case textTypes.PARAGRAPH_SMALL_REGULAR:
+            case textTypes.PARAGRAPH_SMAL_LIGHT:
+                return '10px';
+            case textTypes.PARAGRAPH_BOLD:
+            case textTypes.PARAGRAPH_LIGHT:
+            case textTypes.PARAGRAPH_REGULAR:
             default:
-                return '16px';
+                return '14px';
+        }
+    }, [type]);
+
+    const fontFamily = useMemo(() => {
+        switch (type) {
+            case textTypes.TITLE_BOLD:
+            case textTypes.SUB_TITLE_BOLD:
+            case textTypes.PARAGRAPH_BOLD: 
+            case textTypes.PARAGRAPH_SMALL_BOLD:
+            case textTypes.BUTTON_BOLD:
+                return 'Poppins-Bold';
+            case textTypes.TITLE_LIGHT:
+            case textTypes.SUB_TITLE_LIGHT:
+            case textTypes.PARAGRAPH_LIGHT: 
+            case textTypes.PARAGRAPH_SMAL_LIGHT: 
+            case textTypes.BUTTON_LIGHT: 
+                return 'Poppins-Regular';
+            case textTypes.TITLE_REGULAR:
+            case textTypes.SUB_TITLE_REGULAR:
+            case textTypes.PARAGRAPH_SMALL_REGULAR:
+            case textTypes.PARAGRAPH_REGULAR:
+            case textTypes.BUTTON_REGULAR:
+            default:
+                return 'Poppins-Regular';
         }
     }, [type]);
 
     return (
-        <ContainerText $fontSize={handleSize} $color={color} {...props} />
+        <ContainerText $fontFamily={fontFamily} $fontSize={fontSize} $color={color} {...props} />
     )
 
 };
